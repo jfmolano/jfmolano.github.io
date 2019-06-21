4f3289f1ef90037f15fce45b880fb2c0d854bdec
jfmolano <jose.francisco.molano@gmail.com>
2019-06-21 03:43:19 -0500

    first commit

---
+++ precache-manifest.e3909feec93274e2768556da3e912d55.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7251499a290f805ebc35f7571f481ba5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d02649e3a982b2d08528",
+    "url": "/static/css/main.5deacf59.chunk.css"
+  },
+  {
+    "revision": "9c2e82992fc1855420e3",
+    "url": "/static/js/2.5d11d0c5.chunk.js"
+  },
+  {
+    "revision": "d02649e3a982b2d08528",
+    "url": "/static/js/main.9f9a71fd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
